ea12b2736b73076ee5c05f74d48ac44f44cccdbc
yusuftalhaklc <yusuftalhaklc@gmail.com>
2023-06-21 12:42:47 +0300

    getting started with go gin

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.GET("/", getRoot)
+
+	router.Run("localhost:8080")
+}
+
+func getRoot(context *gin.Context) {
+	context.IndentedJSON(
+		http.StatusOK,
+		gin.H{"message": "Hello Golang"},
+	)
 }
